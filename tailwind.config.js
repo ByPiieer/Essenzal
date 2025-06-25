3d8b75c83f8ed9e30701cc1719ae9614f1b5edfc
ByPiieer <essenzabypier@gmail.com>
2025-06-25 01:11:58 -0400

    CAMBIOS

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: [
+    "./**/*.html",    // escanea todos los HTML en cualquier carpeta
+    "./js/**/*.js"    // escanea todos los JS dentro de la carpeta js
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        playfair: ['Playfair Display', 'serif'],
+      },
+      colors: {
+        brandDark: '#000000',
+        brandLight: '#F5F5F5',
+        brandAccent: '#56DFCF',
+        brandAccentHover: '#FFFBDE',
+        brandPurple: '#ADEED9',
+        brandGold: '#C5A880',
+        brandPinkLight: '#FFEDF3',
+        brandPinkHover: '#e6cdd7',
+
+      },
+    },
+  },
+  plugins: [],
+};
